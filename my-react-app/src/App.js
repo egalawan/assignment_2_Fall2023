3b04d135a84152273b86f02fc5237ae72d2c76b1
egalawan <thisuranawalage@gmail.com>
2023-12-20 00:10:57 -0800

    Got one file name, overall tempo, peak1, peak2

---
+++ my-react-app/src/App.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from 'react';
+
+function App() {
+  const [data, setData] = useState([]);
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const handleFileChange = (event) => {
+    // Update the selected file when the user chooses a file
+    setSelectedFile(event.target.files[0]);
+  };
+
+  const handleUpload = () => {
+    // Check if a file is selected before making the fetch request
+    if (selectedFile) {
+      // Create a FormData object to handle file uploads
+      const formData = new FormData();
+      formData.append('my_audio_file', selectedFile);
+
+      // Make the fetch request with the FormData
+      fetch('http://localhost:8086/api/file_tempo', {
+        method: 'POST',
+        body: formData,
+      })
+        .then((response) => response.json())
+        .then((result) => {
+          setData(result);
+        })
+        .catch((error) => {
+          console.error('Error fetching data:', error);
+        });
+    } else {
+      alert('Please select a file before uploading.');
+    }
+  };
+
+  useEffect(() => {
+    // The useEffect hook runs once when the component mounts
+    // You can perform initial setup here if needed
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Audio Processing App</h1>
+      
+      {/* File Input */}
+      <input type="file" onChange={handleFileChange} />
+      <button onClick={handleUpload}>Upload File</button>
+      
+      {/* Display Overall Tempo */}
+      <p>File Name: {data.filename}</p>
+      <p>Overall Tempo: {data.overall_tempo}</p>
+      <p>Peak#1: {data.peak_1}</p>
+      <p>Peak #2: {data.peak_2}</p>
+      
+      {/* Display other data as needed */}
+    </div>
+  );
+}
+
+export default App;
