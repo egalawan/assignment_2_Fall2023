76eb764c420563db443eb5e27a75015b13a90c26
egalawan <thisuranawalage@gmail.com>
2023-12-20 17:26:52 -0800

    Can select multiple files and editted css to look more clean

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,54 +1,67 @@
 import React, { useState} from 'react';
 
 function App() {
-  const [data, setData] = useState([]);
-  const [selectedFile, setSelectedFile] = useState(null);
-
+  const [data, setData] = useState([]); // Array to store data for multiple files
+  const [selectedFiles, setSelectedFiles] = useState([]); // Array to store multiple files
+  
   const handleFileChange = (event) => {
-    // Update the selected file when the user chooses a file
-    setSelectedFile(event.target.files[0]);
+    // Update the selected files when the user chooses files
+    setSelectedFiles(Array.from(event.target.files));
   };
 
   const handleUpload = () => {
-    // Check if a file is selected before making the fetch request
-    if (selectedFile) {
-      // Create a FormData object to handle file uploads
-      const formData = new FormData();
-      formData.append('my_audio_file', selectedFile);
-
-      // Make the fetch request with the FormData
-      fetch('http://localhost:5000/api/file_tempo', {
-        method: 'POST',
-        body: formData,
-      })
-        .then((response) => response.json())
-        .then((result) => {
-          setData(result);
+    // Check if files are selected before making the fetch requests
+    if (selectedFiles.length > 0) {
+      selectedFiles.forEach(file => {
+        const formData = new FormData();
+        formData.append('my_audio_file', file);
+  
+        // Make the fetch request with the FormData for each file
+        fetch('http://localhost:5000/api/file_tempo', {
+          method: 'POST',
+          body: formData,
         })
-        .catch((error) => {
+        .then(response => response.json())
+        .then(result => {
+          setData(prevData => [...prevData, result]);
+        })
+        .catch(error => {
           console.error('Error fetching data:', error);
         });
+      });
+  
+      // Clear the selected files after upload
+      setSelectedFiles([]);
     } else {
-      alert('Please select a file before uploading.');
+      alert('Please select files before uploading.');
     }
   };
-
 
   return (
     <div className="App">
       <h1>Audio Processing App</h1>
       
-      {/* File Input */}
-      <input type="file" onChange={handleFileChange} />
-      <button onClick={handleUpload}>Upload File</button>
+      <div className="file-input-container">
+        <input type="file" onChange={handleFileChange} multiple />
+        <button className="upload-btn" onClick={handleUpload}>Upload Files</button>
+      </div>
       
-      {/* Display Overall Tempo */}
+      {data.map((fileData, index) => (
+        <FileInfoCard key={index} data={fileData} />
+      ))}
+    </div>
+  );
+  
+}
+
+function FileInfoCard({ data }) {
+  return (
+    <div className="file-info-card">
+      <h2>File Information</h2>
       <p>File Name: {data.filename}</p>
       <p>Overall Tempo: {data.overall_tempo}</p>
-      <p>Peak#1: {data.peak_1}</p>
+      <p>Peak #1: {data.peak_1}</p>
       <p>Peak #2: {data.peak_2}</p>
-      
-      {/* Display other data as needed */}
     </div>
   );
 }
